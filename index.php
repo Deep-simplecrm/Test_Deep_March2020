b6901de8ae933b81681dd66e3d3041dcc06e500d
deep-khode <deep@simplecrm.com.sg>
2020-03-14 18:03:46 +0530

    index.php

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
+	
+	error_reporting(0);
+	include "includes/header.php";
 
-echo "Hello World";
+	require_once __DIR__ . '/vendor/autoload.php';
+
+	if(isset($_GET['view'])){
+		if($_GET['view'] == 'login'){
+			include "includes/login.php";
+		}elseif($_GET['view'] == 'download_file'){
+			include "includes/download_file.php";
+		}
+	}else{
+		include "includes/register.php";
+	}
+
+
+	include "includes/footer.php";
 
 ?>
